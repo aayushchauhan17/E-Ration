1f19749f58968495e2e1340b398a8a19dbe5920d
aayushchauhan17 <aayushchauhan176@gmail.com>
2023-05-08 00:35:17 +0530

    delivery page added

--- src/pages/DeliveryPage/DeliveryPage.js
+++ src/pages/DeliveryPage/DeliveryPage.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { SafeAreaView, Text, View } from "react-native";
 import { HeaderMainPage } from "../mainPageComp/HeaderMainPage";
+import CustomerDetails from "../ThankyouPage/CostumerDetails";
+import OrderDetails from "../ThankyouPage/OrderDetails";
+import { ScrollView } from "react-native";
+import { TouchableOpacity } from "react-native";
 
 function DeliveryPage({ navigation, route }) {
   const { oderDeliverData } = route.params;
@@ -8,7 +12,41 @@ function DeliveryPage({ navigation, route }) {
   return (
     <SafeAreaView style={{ backgroundColor: "white", height: "100%" }}>
       <HeaderMainPage otherPage={true} />
-      <Text>Hi delivery page</Text>
+      <ScrollView>
+        <CustomerDetails userData={oderDeliverData?.userData} />
+        <OrderDetails cart={oderDeliverData?.cartData} />
+
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            marginVertical: 20,
+            justifyContent: "space-around",
+          }}
+        >
+          <Text style={{ fontSize: 16, fontWeight: "400" }}>
+            Deliver this product :{" "}
+          </Text>
+          <TouchableOpacity onPress={() => {}}>
+            <View
+              style={{
+                width: 150,
+                paddingHorizontal: 7,
+                paddingVertical: 6,
+                backgroundColor: "lightgreen",
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 5,
+              }}
+            >
+              <Text style={{ fontSize: 17, fontWeight: "500" }}>
+                Deliver Now
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
