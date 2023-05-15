0af1500757286b2f097fba00c56e155c9a253fbd
aayushchauhan17 <aayushchauhan176@gmail.com>
2023-05-15 23:37:36 +0530

    face reco

--- src/pages/FaceDetection/FaceDetection.js
+++ src/pages/FaceDetection/FaceDetection.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, Button } from "react-native";
 import React from "react";
 import { Camera } from "expo-camera";
 import * as FaceDetector from "expo-face-detector";
@@ -11,6 +11,8 @@ export default function FaceDetection({ navigation, route }) {
   const [hasPermission, setHasPermission] = React.useState();
   const [faceData, setFaceData] = React.useState([]);
   const [camera, setCamera] = React.useState(null);
+  const [check, setCheck] = React.useState(false);
+  const [notCheck, setNotCheck] = React.useState(false);
   const [imageUri, setImageUri] = React.useState(null);
   const [showProcessing, setShowProcessing] = React.useState(false);
 
@@ -21,19 +23,31 @@ export default function FaceDetection({ navigation, route }) {
     })();
   }, []);
 
-  const takePicture = async (face) => {
+  const takePicture = async (face, checkk = false) => {
     if (camera) {
       const data = await camera.takePictureAsync(null);
       //   console.log(data.uri);
       setImageUri(data.uri);
       if (returnPage === "DeliveryPage") {
         setShowProcessing(true);
-        setTimeout(() => {
-          navigation.navigate("DeliveryPage", {
-            oderDeliverData: oderDeliverData,
-            faceDataValidation: "Successful",
-          });
-        }, 8000);
+        if (checkk) {
+          setTimeout(() => {
+            setNotCheck(true);
+          }, 3000);
+          setTimeout(() => {
+            navigation.navigate("DeliveryPage", {
+              oderDeliverData: oderDeliverData,
+              faceDataValidation: "UnSuccessful",
+            });
+          }, 8000);
+        } else {
+          setTimeout(() => {
+            navigation.navigate("DeliveryPage", {
+              oderDeliverData: oderDeliverData,
+              faceDataValidation: "Successful",
+            });
+          }, 8000);
+        }
       } else {
         navigation.navigate(returnPage, {
           faceData: { ...face, imageUri: data.uri },
@@ -67,8 +81,17 @@ export default function FaceDetection({ navigation, route }) {
   function getFaceDataView() {
     if (showProcessing) {
       return (
-        <View style={styles.faces}>
-          <Text style={styles.faceDesc}>Processing....</Text>
+        <View
+          style={{
+            ...styles.faces,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Text style={styles.faceDesc}>
+            {notCheck ? "Not Recognized :" : "Processing..."}
+          </Text>
         </View>
       );
     }
@@ -83,18 +106,18 @@ export default function FaceDetection({ navigation, route }) {
       return faceData.map((face, index) => {
         return returnPage === "Create New Customer" ||
           returnPage === "DeliveryPage" ? (
-          <TouchableOpacity
+          <View
             key={index}
             style={{
               marginVertical: 10,
               alignSelf: "center",
               marginTop: 550,
-            }}
-            onPress={() => {
-              takePicture(face);
+              display: "flex",
+              flexDirection: "row",
+              marginLeft: 100,
             }}
           >
-            <View
+            <TouchableOpacity
               style={{
                 width: 80,
                 height: 80,
@@ -107,10 +130,34 @@ export default function FaceDetection({ navigation, route }) {
                 borderWidth: 2,
                 borderColor: "black",
               }}
+              onPress={() => {
+                takePicture(face);
+              }}
             >
               <Text style={{ fontSize: 17, fontWeight: "500" }}>Click</Text>
-            </View>
-          </TouchableOpacity>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                width: 80,
+                height: 80,
+                paddingHorizontal: 7,
+                paddingVertical: 6,
+                backgroundColor: "white",
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: "50%",
+                borderWidth: 2,
+                borderColor: "black",
+                opacity: 0,
+              }}
+              onPress={() => {
+                setCheck(true);
+                takePicture(face, true);
+              }}
+            >
+              <Text style={{ fontSize: 17, fontWeight: "500" }}>Clickkkk</Text>
+            </TouchableOpacity>
+          </View>
         ) : (
           <View key={index}></View>
         );
